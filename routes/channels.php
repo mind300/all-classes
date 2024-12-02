8f76eb74c92ed9e678365667f451658289667b7d
khaledmoussaaa <khaledmoussa909@gmail.com>
2024-12-02 15:37:21 +0200

    Fix route of channel and security

--- routes/channels.php
+++ routes/channels.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Broadcast;
 
-Broadcast::channel('testing', function ($user) {
-    return true;
+Broadcast::channel('chat.{chatId}', function ($user, $chatId) {
+    // Example: Check if the user is part of the chat room
+    return $user->chats()->where('id', $chatId)->exists();
 });
